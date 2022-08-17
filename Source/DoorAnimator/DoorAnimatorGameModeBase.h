76c09f28593eab0c943a2dd9197b30b84af91b9b
Edith Flores <Edith.Flores@activision.com>
2022-08-17 15:57:20 -0400

    Add project files.

---
+++ Source/DoorAnimator/DoorAnimatorGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "DoorAnimatorGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class DOORANIMATOR_API ADoorAnimatorGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
